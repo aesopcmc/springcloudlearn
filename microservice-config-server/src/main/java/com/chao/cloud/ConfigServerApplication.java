e4f558f818c66cf83b6b0ecd0d3ca0c583e7aef0
aesopcmc <chao_c_c@163.com>
2019-06-29 18:16:25 +0800

    bbbb

---
+++ microservice-config-server/src/main/java/com/chao/cloud/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.chao.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author AESOP
+ * @date 2019/6/29 10:30
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
---
+++ microservice-discovery-eureka/src/main/java/com/chao/cloud/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.chao.cloud;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Spring Cloud Finchley及更高版本，必须添加如下代码，部分关闭掉Spring Security
+ * 的CSRF保护功能，否则应用无法正常注册！
+ * ref: http://cloud.spring.io/spring-cloud-netflix/single/spring-cloud-netflix.html#_securing_the_eureka_server
+ * @author AESOP
+ * @date 2019/6/29 17:37
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
